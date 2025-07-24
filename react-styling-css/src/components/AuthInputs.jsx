08763a8c26d530ae19c33e259f3ae1e0e25c5301
harisvelispahic <haris.velispahic@gmail.com>
2025-07-24 02:14:52 +0200

    Add react-styling-css

---
+++ react-styling-css/src/components/AuthInputs.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === "email") {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes("@");
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <StyledDiv>
+        <Input
+          label={"Email"}
+          invalid={emailNotValid}
+          type="email"
+          $invalid={emailNotValid}
+          // className={emailNotValid ? "invalid" : undefined}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          label={"Password"}
+          invalid={passwordNotValid}
+          type="password"
+          $invalid={passwordNotValid}
+          // className={passwordNotValid ? "invalid" : undefined}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+      </StyledDiv>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <Button /*className="button"*/ onClick={handleLogin}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
