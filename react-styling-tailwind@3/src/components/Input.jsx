89d891b7d766ba4976414bb8419a20a3160278a5
harisvelispahic <haris.velispahic@gmail.com>
2025-07-25 11:27:52 +0200

    Add react-styling-tailwind@3

---
+++ react-styling-tailwind@3/src/components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ label, invalid, ...props }) {
+  let labelClasses = `block mb-2 text-xs font-bold tracking-wide uppercase ${
+    invalid ? "text-red-400" : "text-stone-300"
+  }`;
+  let inputClasses = `w-full px-3 py-2 leading-tight border rounded shadow ${
+    invalid
+      ? "text-red-500 bg-red-100 border-red-300"
+      : "text-gray-700 bg-stone-300"
+  }`;
+
+  return (
+    <p>
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
+    </p>
+  );
+}
