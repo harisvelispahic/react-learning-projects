d487a0d3f34724af8a31044a816af89aed1c2fa9
harisvelispahic <haris.velispahic@gmail.com>
2025-08-24 17:00:24 +0200

    Add react-custom-hooks

---
+++ react-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(fetchFn, initialValue) {
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const places = await fetchFn();
+        setFetchedData(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    fetchedData,
+    setFetchedData,
+    isFetching,
+    error,
+  };
+}
