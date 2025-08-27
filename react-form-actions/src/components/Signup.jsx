6682638d1084ff087fb4d5dbeb089915ed0a47f4
harisvelispahic <haris.velispahic@gmail.com>
2025-08-28 00:13:41 +0200

    Add react-form-actions

---
+++ react-form-actions/src/components/Signup.jsx
@@ -0,0 +1,183 @@
+import { useActionState } from "react";
+import { isEmpty, isEmail, hasMinLength } from "../util/validation";
+
+function submitAction(prevFormState, formState) {
+  const email = formState.get("email");
+  const password = formState.get("password");
+  const confirmPassword = formState.get("confirm-password");
+  const firstName = formState.get("first-name");
+  const lastName = formState.get("last-name");
+  const role = formState.get("role");
+  const terms = formState.get("terms");
+  const acquisitionChannel = formState.getAll("acquisition");
+
+  const errors = [];
+
+  if (!isEmail(email)) {
+    errors.push("Please enter a valid email address.");
+  }
+  if (isEmpty(password) || !hasMinLength(password, 6)) {
+    errors.push("Password must be at least 6 characters long.");
+  }
+  if (password !== confirmPassword) {
+    errors.push("Passwords do not match.");
+  }
+  if (isEmpty(firstName) || isEmpty(lastName)) {
+    errors.push("Please enter your first and last name.");
+  }
+  if (isEmpty(role)) {
+    errors.push("Please select your role.");
+  }
+  if (!terms) {
+    errors.push("You must agree to the terms and conditions.");
+  }
+  if (acquisitionChannel.length === 0) {
+    errors.push("Please select at least one acquisition channel.");
+  }
+
+  if (errors.length > 0) {
+    return {
+      errors,
+      enteredValues: {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName,
+        role,
+        terms,
+        acquisitionChannel,
+      },
+    };
+  }
+  return { errors: null };
+}
+
+export default function Signup() {
+  const [formState, formAction, pending] = useActionState(submitAction, { errors: null });
+  // Used for managing form state and actions
+  // Returns formState, formAction, and pending state
+  // formState contains the current state of the form
+  // formAction is the function to call when submitting the form,
+  //    it is the "submitAction" function, but wrapped by another function and react is aware of it,
+  //    because of that we use it as a value for the "action" attribute of the form
+  // pending is a boolean indicating if the form is currently being submitted
+  //    we are not using it in this example, but it can be useful for showing loading states (async operations)
+  return (
+    <form action={formAction}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" defaultValue={formState.enteredValues?.password} />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName} />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName} />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role" defaultValue={formState.enteredValues?.role}>
+          <option value="">-- Select a role --</option>
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes("google")}
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes("friend")}
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes("other")}
+          />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formAction.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button className="button">Sign up</button>
+      </p>
+    </form>
+  );
+}
